fee581e73d9006c6ffc6c580b8b47f3e06c5123d
wily123 <vil_soni@hotmail.com>
2014-07-28 14:26:36 +0200

    Update login.php

--- login.php
+++ login.php
@@ -28,31 +28,29 @@ if(!$_SESSION['pasi'] || $_SESSION['pasi']!= $password ){
         
 		session_destroy();	
 		
-		if(isset($_POST['pasi'])){ $errMsg = true;
-		$_SESSION['pasi']=$_POST['pasi'];
-		} 
+		if(isset($_POST['pasi'])){
+			$errMsg = true;
+			$_SESSION['pasi']=$_POST['pasi'];
+			} 
 		
 		if($_SESSION['pasi']!= $password){
-		echo "
-		<style>
-		body{ text-aling:center; margin:30px auto; background:#606060; }
-		.errorLogin{ color:#f33; margin:10px; font-size: 17pt; background:rgba(128,128,128, .7); border-radius: 4px; padding:4px 20px;display:inline-block}
-		.login input { font-size: 17pt; border: 1px solid #FFF; display: inline-block; border-radius: 4px; 
-		margin: 4px; padding: 10px; color: #FFF; background: none repeat scroll 0% 0% #0C94D4;}
-		</style>
-		<title>Login</title>
-		<body><center>
-		<form action=''  class='login' method='post'>";
-		if($errMsg){ echo "<span class='errorLogin'>".$error."</span><br />";}
-		echo "<input type='password' id='pasi' placeholder='Password' name='pasi' value=''><input  width='auto' type='submit' value='Login'>
-		</form></center></body>";
-		exit();	
-		}
-		
-		 
+			echo "
+			<style>
+			body{ text-aling:center; margin:30px auto; background:#606060; }
+			.errorLogin{ color:#f33; margin:10px; font-size: 17pt; background:rgba(128,128,128, .7); border-radius: 4px; padding:2px 20px;display:inline-block}
+			.login input { font-size: 17pt; border: 1px solid #FFF; display: inline-block; border-radius: 4px; 
+			margin: 4px; padding: 10px; color: #FFF; background: none repeat scroll 0% 0% #0C94D4;}
+			</style>
+			<title>Login</title>
+			<body><center>
+			<form action=''  class='login' method='post'>";
+			if($errMsg){ echo "<span class='errorLogin'>".$error."</span><br />";}
+			echo "<input type='password' id='pasi' placeholder='Password' name='pasi' value=''><input  width='auto' type='submit' value='Login'>
+			</form></center></body>";
+			exit();	
+			} 
 	
 }
-
 
 
 
